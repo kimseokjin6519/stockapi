fd9cb45164c7661dcdd83ecd8221e9c9fdcb89f1
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-12-14 00:11:12 +0900

    Updates - Server.js

--- server.js
+++ server.js
@@ -7,29 +7,42 @@ const PORT = 5000;
 
 app.use(cors());
 
-app.get('/quote', async (req, res) => {
+app.get('/chart', async (req, res) => {
    const { symbol } = req.query;
-
-   if (!symbol) {
+   if (!symbol)
       return res.status(400).json({ error: 'Symbol is required' });
-   }
 
-   try {
-   
+   try {   
+      
+      /* 3 Months */
       const endDate = new Date();
       const startDate = new Date();
       startDate.setMonth(endDate.getMonth() - 3);
-
       const period1 = startDate.toISOString().split('T')[0];
       const period2 = endDate.toISOString().split('T')[0];
-
+      
+      /* Fetch Chart Data for Symbol */
       const chartData = await yf.chart(symbol, { period1, period2 });
-
+      
+      /* Return Chart Data for Symbol */
       res.json(chartData);
 
-   } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch stock data', message: error.message });
-   }
+   } catch (error) { res.status(500).json({ error: 'Failed to fetch stock data', message: error.message }) }
+});
+
+app.get('/quote', async (req, res) => {
+   const { symbol } = req.query;
+   if (!symbol) 
+      return res.status(400).json({ error: 'Symbol is required' });
+   try {
+
+      /* Fetch Financials Data */
+      const allData = await yf.quote(symbol);
+
+      /* Return Financials Data for Symbol */ 
+      res.json(allData);
+
+   } catch (error) { res.status(500).json({ error: 'Failed to fetch stock data', message: error.message }) }
 });
 
 app.listen(PORT, () => {
